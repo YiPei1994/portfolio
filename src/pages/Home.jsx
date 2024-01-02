918b4cd0fb5e3727c897a46503975b4927a313f8
yipei <studentypz@gmail.com>
2024-01-02 14:05:35 +0100

    footer + home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,26 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="px-9 py-4 text-center">
+      <div className="mainBlock my-4">
+        <h1 className="mb-4 text-3xl leading-normal		">
+          Hey, I&apos;m{' '}
+          <span className="uppercase text-yellow-500">Yi Pei Zhu</span>,
+          <br />
+          your
+          <span className="upppercase text-yellow-500"> Frontend Maestro</span>!
+        </h1>
+        <p>
+          A junior to mid-level developer driven by passion and a constant
+          desire for self-improvement. Infusing every project with energy and
+          creativity, I&apos;m here to redefine the possibilities of frontend
+          development. Let&apos;s craft digital dreams together! 🚀✨
+        </p>
+        <button className="my-8 rounded-xl bg-yellow-500 px-6 py-3 hover:bg-yellow-600">
+          Hire Me
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
